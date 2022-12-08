8d053a895b5b7d757d33f1dddff4164d23873ab1
albertbgo <116098317+albertbgo@users.noreply.github.com>
2022-12-08 10:20:52 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -11,9 +11,10 @@ sudo echo 'JAVA_HOME="/usr/lib/jvm/java-1.11.0-openjdk-amd64/bin"'  | sudo tee -
 source /etc/environment
 echo $JAVA_HOME
 
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.69/bin/apache-tomcat-9.0.69.tar.gz
-sudo tar xzvf apache-tomcat-9.0.69.tar.gz
-sudo mv apache-tomcat-9.0.69 /opt/tomcat/
+
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.70/bin/apache-tomcat-9.0.70.tar.gz
+sudo tar xzvf apache-tomcat-9.0.70.tar.gz
+sudo mv apache-tomcat-9.0.70 /opt/tomcat/
 sudo chown -R www-data:www-data /opt/tomcat/
 sudo chmod -R 777 /opt/tomcat/
 
